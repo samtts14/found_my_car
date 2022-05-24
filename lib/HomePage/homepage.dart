f7fa44fb0dc981d20c1896888ac52d88d6f62e29
Samuel <samyts1497@gmail.com>
2022-05-24 10:58:12 -0800

    Correccion de escritura en nombre de carpeta

--- lib/HomePage/homepage.dart
+++ lib/HomePage/homepage.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:found_my_car/BehiculosPages/vehiculo_page1.dart';
-import 'package:found_my_car/BehiculosPages/vehiculo_page2.dart';
+import 'package:found_my_car/VehiculosPages/vehiculo_page1.dart';
+import 'package:found_my_car/VehiculosPages/vehiculo_page2.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
 
 class HomePage extends StatelessWidget {
   final _controller = PageController();
@@ -19,18 +18,20 @@ class HomePage extends StatelessWidget {
             height: 500,
             child: PageView(
               controller: _controller,
-              children: const[
+              children: const [
                 Page1(),
                 Page2(),
               ],
             ),
           ),
           SmoothPageIndicator(
-            controller: _controller, 
+            controller: _controller,
             count: 2,
             effect: ExpandingDotsEffect(
-              activeDotColor: Colors.blue,//------------COLOR DEL SELECTOR-----------------
-              dotColor: Colors.brown,//----------------COLOR DE LA SIGUIENTE PAGINA------------
+              activeDotColor:
+                  Colors.blue, //------------COLOR DEL SELECTOR-----------------
+              dotColor: Colors
+                  .brown, //----------------COLOR DE LA SIGUIENTE PAGINA------------
               dotHeight: 30,
               dotWidth: 30,
               spacing: 16,
