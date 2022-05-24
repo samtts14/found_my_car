ed615f69dcc08a47c323a41491009b71d2e41915
Samuel <samueltoribio.santos@gmail.com>
2022-05-23 22:00:17 -0400

    Inicio de nueva interfaz
    Inicio de un nuevo swipe view

---
+++ lib/BehiculosPages/vehiculo_page1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: SafeArea(
+        //padding: const EdgeInsets.all(20.0),
+        child: Container(color: Colors.red),
+      ),
+    );
+  }
+}
