49e7b42b47313756f5c4b71b0f1ef1536ed63202
Samuel <samueltoribio.santos@gmail.com>
2022-05-23 22:00:17 -0400

    Inicio de nueva interfaz

---
+++ android/app/src/main/kotlin/com/example/found_my_car/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.found_my_car
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/HomePage/homepage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:found_my_car/BehiculosPages/vehiculo_page1.dart';
+import 'package:found_my_car/BehiculosPages/vehiculo_page2.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+
+class HomePage extends StatelessWidget {
+  final _controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // Page View
+          SizedBox(
+            height: 500,
+            child: PageView(
+              controller: _controller,
+              children: const[
+                Page1(),
+                Page2(),
+              ],
+            ),
+          ),
+          SmoothPageIndicator(
+            controller: _controller, 
+            count: 2,
+            effect: ExpandingDotsEffect(
+              activeDotColor: Colors.blue,//------------COLOR DEL SELECTOR-----------------
+              dotColor: Colors.brown,//----------------COLOR DE LA SIGUIENTE PAGINA------------
+              dotHeight: 30,
+              dotWidth: 30,
+              spacing: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
